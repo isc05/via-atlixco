93d9d317258e0451b871d7b5d538b2aa71379c59
isc05 <isc242105@itsatlixco.edu.mx>
2026-09-20 13:59:02 -0600

    Probando creación de componentes 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 //non exports
+import TemporalButton from "@/components/ui/TemporalButton";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -9,9 +15,9 @@ export default function Home() {
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
       </div>
-
       <div className="relative flex place-items-center before:absolute before:h-75 before:w-120 before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-45 after:w-60 after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:to-transparent after:blur-2xl after:content-[''] min-h-screen flex-col items-center justify-between p-24">
         <h1 className="text-4xl font-bold mb-4">Vía Atlixco</h1>
+        <TemporalButton onClick={() => router.push("/profile")} />
         <p className="text-lg text-gray-600 dark:text-gray-400">
           En desarrollo
         </p>
---
+++ components/ui/TemporalButton.tsx
@@ -0,0 +1,7 @@
+export default function TemporalButton({ onClick }: { onClick: () => void }) {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={onClick}>
+      Profile
+    </button>
+  );
+}
